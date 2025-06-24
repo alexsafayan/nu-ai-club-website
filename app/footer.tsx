9ca8c0bd68b03c82614f90f6176c8b2f89433bc8
Alex Safayan <alex@safayan.com>
2025-06-24 10:58:53 -0700

    nit

--- app/footer.tsx
+++ app/footer.tsx
@@ -69,11 +69,11 @@ function ThemeSwitch() {
 export function Footer() {
     return (
         <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between gap-x-4">
                 <div className="text-xs text-zinc-500">
                     © {new Date().getFullYear()} Northwestern AI Club. Not affiliated with or endorsed by Northwestern University.
                 </div>
-                <div className="text-xs text-zinc-400">
+                <div className="shrink-0 text-xs text-zinc-400">
                     <ThemeSwitch />
                 </div>
             </div>
